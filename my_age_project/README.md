22a84126815f632f6ed711d6a7e07c4fc5dfd8a8
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-03 00:45:18 +0300

    changed readme

--- my_age_project/README.md
+++ my_age_project/README.md
@@ -1,2 +1,36 @@
 A sample command-line application with an entrypoint in `bin/`, library code
 in `lib/`, and example unit test in `test/`.
+
+this is just to practice my skills
+___
+
+# this is a heading
+## this is a subheading
+
+
+*item one
+1. one
+2. two 
+3. three
+
+> as you can see this is way cooler that I thought
+
+[Link](github.com/Dazhyar)
+[Link][1]
+
+___
+
+`print("test");`
+
+#**[Link](https://github.com/Dazhyar)**
+
+
+```
+void main() {
+    print("Hello, World!")
+}
+```
+
+
+[1]: github.com/dazhyar
+
--- my_age_project/bin/my_age_project.dart
+++ my_age_project/bin/my_age_project.dart
@@ -5,9 +5,46 @@ void main(List<String> arguments) {
   const b = -5;
   const c = 4;
 
+  dynamic? d;
+  d = 5;
+  print(d);
+  d = "stirngfasdf";
+  print(d);
+
   // Will return a NaN value if the value (b^2 - 4ac) returns a negative number
-  /*
+  /**
     this is just to practice my skills
+    ___
+
+    # this is a heading
+    ## this is a subheading
+
+
+    *item one
+    1. one
+    2. two 
+    3. three
+
+    > as you can see this is way cooler that I thought
+
+    [Link](github.com/Dazhyar)
+    [Link][1]
+
+    ___
+
+    `print("test");`
+
+    #**[Link](https://github.com/Dazhyar)**
+
+
+   ```
+    void main() {
+      print("Hello, World!")
+    }
+   ```
+
+
+    [1]: github.com/dazhyar
 
    */
   double x1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
