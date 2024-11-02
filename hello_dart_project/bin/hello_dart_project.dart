fa392fa72e901622d30c5f8b414a08a666b89d93
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-02 23:25:41 +0300

    Init commit

---
+++ hello_dart_project/bin/hello_dart_project.dart
@@ -0,0 +1,13 @@
+import 'package:hello_dart_project/hello_dart_project.dart'
+    as hello_dart_project;
+import 'dart:math';
+
+void main(List<String> arguments) {
+  int number = 10;
+  number = 15;
+
+  double apple = 3.14159;
+
+  print(9.isEven);
+  print(3.14159.round());
+}
---
+++ my_age_project/bin/my_age_project.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  var age = 30;
+  print(age.runtimeType);
+  age = 30;
+  print(age.runtimeType);
+  print(age.runtimeType);
+}
