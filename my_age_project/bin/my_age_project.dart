f664409b96023742f9cbdefa84a362d10612ed50
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-03 00:20:13 +0300

    finished chapter 2

--- my_age_project/bin/my_age_project.dart
+++ my_age_project/bin/my_age_project.dart
@@ -1,7 +1,18 @@
+import 'dart:math';
+
 void main(List<String> arguments) {
-  var age = 30;
-  print(age.runtimeType);
-  age = 30;
-  print(age.runtimeType);
-  print(age.runtimeType);
+  const a = 1;
+  const b = -5;
+  const c = 4;
+
+  // Will return a NaN value if the value (b^2 - 4ac) returns a negative number
+  /*
+    this is just to practice my skills
+
+   */
+  double x1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
+  double x2 = (-b - sqrt(b*b - 4*a*c)) / (2*a);
+
+  print(x1);
+  print(x2);
 }
