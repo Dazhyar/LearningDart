4a858d7febdd33ae5a9c050c51cd049434c2fa0a
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-05 22:43:21 +0300

    finished if, switch, and enums

--- chapter4/bin/chapter4.dart
+++ chapter4/bin/chapter4.dart
@@ -1,3 +1,20 @@
+const global = 'Hello, world';
+
 void main(List<String> arguments) {
-  print('Hello world: ${chapter4.calculate()}!');
+ const audioState = AudioState.stopped;
+
+ switch (audioState) {
+  case AudioState.playing:
+  print('playing');
+  case AudioState.paused:
+  print('paused');
+  case AudioState.stopped:
+  print('stopped');
+ }
+}
+
+enum AudioState {
+  playing,
+  paused,
+  stopped,
 }
