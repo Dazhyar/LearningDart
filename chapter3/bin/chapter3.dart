0768ee566a38090f9986d916d93f211f61772aeb
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-05 21:25:30 +0300

    end of chapter 3

--- chapter3/bin/chapter3.dart
+++ chapter3/bin/chapter3.dart
@@ -1,21 +1,8 @@
 import 'package:characters/characters.dart';
 
 void main(List<String> arguments) {
-  var greeting = 'Hello' + 'My name is ';
-
-  final message = StringBuffer();
-
-  message.write('hello');
-  message.write(' my name is ');
-  message.write('ray');
-
-  print(message.toString());
-  print(message.isEmpty.toString() + message.length.toString());
-  message.clear();
-  print(message.isEmpty.toString() + message.length.toString());
-
-  int a = 4;
-  print(a.toString());
-  double b = 4;
-  print(b.toString());
+  const number = 10;
+const multiplier = 5;
+final summary = '$number \u00D7 $multiplier = ${number * multiplier}';
+print(summary);
 }
