a7c539652c768b6800b466a088953a624b9adab5
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-06 20:36:55 +0300

    finished chapter5

--- chapter5/bin/chapter5.dart
+++ chapter5/bin/chapter5.dart
@@ -1,11 +1,32 @@
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:chapter5/chapter5.dart' as chapter5;
 
 void main(List<String> arguments) {
-  const input = 12;
-  final output = compliment(input);
-  print(output);
+  print(repeatTask(4, 2, (int num) => num*num));
 }
 
-String compliment(int number) {
-  return '$number is a very nice number';
+ 
+int repeatTask(int times, int input, Function task) {
+  int result = task(input);
+  for (int i = 0; i < times-1; i++) {
+    result = task(result);
+  }
+
+  return result;
+}
+
+bool checkPrime(int number) {
+  if (number == 1) {
+    return false;
+  }
+  
+  for (int i = 2; i < number; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
