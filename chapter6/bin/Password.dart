16efe5c337da3526229a76e720b5eeedb800cc8c
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-11 20:49:33 +0300

    added Password class

---
+++ chapter6/bin/Password.dart
@@ -0,0 +1,16 @@
+class Password {
+  String value = '';
+
+  bool isValid() {
+    if (value.length > 8) {
+      return true;
+    }
+
+    return false;
+  }
+
+  @override
+  String toString() {
+    return value;
+  }
+}
--- chapter6/bin/chapter6.dart
+++ chapter6/bin/chapter6.dart
@@ -1,9 +1,16 @@
 import 'user.dart';
+import 'Password.dart';
 
 void main() {
-  final user = User()
-  ..name = 'Ray'
-  ..id = 42;
-  print(user);
-  print(user.toJson());
+  final pass = Password()
+  ..value = '12345678';
+
+  print(pass);
+  print(pass.isValid());
+
+  final pass2 = Password()
+  ..value = '123456789';
+
+  print(pass2);
+  print(pass2.isValid());
 }
