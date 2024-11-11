6698603a9ef5f26af04e24004f6192ac05fb24fa
Dazhyar <dazhyarzanyar@gmail.com>
2024-11-11 21:06:28 +0300

    added long form, generative constructor for User

--- chapter6/bin/chapter6.dart
+++ chapter6/bin/chapter6.dart
@@ -2,15 +2,6 @@ import 'user.dart';
 import 'Password.dart';
 
 void main() {
-  final pass = Password()
-  ..value = '12345678';
-
-  print(pass);
-  print(pass.isValid());
-
-  final pass2 = Password()
-  ..value = '123456789';
-
-  print(pass2);
-  print(pass2.isValid());
+  final user = User();
+  print(user);
 }
--- chapter6/bin/user.dart
+++ chapter6/bin/user.dart
@@ -1,6 +1,22 @@
 class User {
+  // long form
+  // generative constructor: generates an object of the same type.
+  
+  /*
+    having the same name for the properties, and the constructor's
+    parameteres is called shadowing.
+    Ex: this.name (object property) and name (parameter).
+   */
+  User(int id, String name) {
+    this.id = id;
+    this.name = name; 
+  }
+
+
   int id = 0;
   String name = '';
+
+  User();
 
   String toJson() {
     return '{"id": $id, "name": "$name"}';
